6bc66bc2a691aff40ef8ac28e50a82b2122f7225
Ahmed Shehab <43203303+Shohob@users.noreply.github.com>
2020-08-03 05:01:14 +0200

    Update ad.py

--- ad.py
+++ ad.py
@@ -1,17 +1,10 @@
 # Uses Python 3
-
-
 def Max(a,b):
     count =0
     for i in range(0,n):
         count+=(a[i]*b[i])
    
     return count
-
-
-
-
-
 
 n = int(input())
 a = list(map(int,input().split()))
